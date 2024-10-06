3ad1457243cc8d620a1c5a68498249aa9c0ef783
mohammedabarh <abmohammed606@gmail.com>
2024-10-05 20:06:55 -0700

    RSA-Factoring-Challenge

--- factors.c
+++ factors.c
@@ -1,29 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * compute_factors - Find and print factors of a number
+ * @target: The number to factorize
+ *
+ * This function finds the smallest factor of the target number
+ * and prints the factorization.
+ */
+void compute_factors(long long int target)
+{
+	long int divisor;
+	long long int quotient;
+
+	for (divisor = 2; divisor <= sqrt(target); divisor++)
+	{
+		if (target % divisor == 0)
+		{
+			quotient = target / divisor;
+			printf("%lld=%lld*%ld\n", target, quotient, divisor);
+			return;
+		}
+	}
+	printf("%lld=%lld*1\n", target, target);
+}
 
 /**
  * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
  *
- * Return: 0 on success, -1 on failure
+ * Return: 0 on success, 1 on failure
  */
-int main(void)
+int main(int argc, char *argv[])
 {
-    long long int target_number = 239809320265259;
-    long int smaller_factor = 2;
-    long int larger_factor;
+	FILE *input_file;
+	long long int current_number;
 
-    while (target_number % smaller_factor)
-    {
-        if (smaller_factor <= target_number)
-        {
-            smaller_factor++;
-        }
-        else
-        {
-            return (-1);
-        }
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return (1);
+	}
 
-    larger_factor = target_number / smaller_factor;
-    printf("%lld = %ld * %ld\n", target_number, larger_factor, smaller_factor);
-    return (0);
+	input_file = fopen(argv[1], "r");
+	if (!input_file)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+
+	while (fscanf(input_file, "%lld", &current_number) == 1)
+	{
+		compute_factors(current_number);
+	}
+
+	fclose(input_file);
+	return (0);
 }
