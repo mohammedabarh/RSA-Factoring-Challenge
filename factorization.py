d1621459ecde26bf1ba0cdfbd0c9d2aff5559c1e
mohammedabarh <abmohammed606@gmail.com>
2024-10-05 19:46:16 -0700

    RSA-Factoring-Challenge

--- factorization.py
+++ factorization.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-import sys
+"""Module for RSA factorization"""
+from sys import argv
 
 
-def find_two_factors(target):
-    """Generate two factors for a given number"""
-    divisor = 2
-    while (target % divisor):
-        if (divisor <= target):
-            divisor += 1
+def factorize_rsa(file_path):
+    """
+    Factorize numbers from a file using RSA algorithm.
 
-    quotient = target // divisor
-    return (quotient, divisor)
+    Args:
+        file_path (str): Path to the input file.
+    """
+    with open(file_path) as input_file:
+        for line in input_file:
+            number = int(line)
+            print(f"{number:d}=", end="")
+            if number % 2 == 0:
+                print(f"{number//2}*2")
+                continue
+            for divisor in range(3, number, 2):
+                if number % divisor == 0:
+                    quotient = number // divisor
+                    for potential_factor in range(3, quotient, 2):
+                        if quotient % potential_factor == 0 or divisor % potential_factor == 0:
+                            break
+                    print(f"{quotient}*{divisor}")
+                    break
 
 
-if len(sys.argv) != 2:
-    sys.exit(f"Incorrect usage: {sys.argv[0]} <input_file_path>")
-
-input_file_path = sys.argv[1]
-
-with open(input_file_path, 'r') as file:
-    content = file.readlines()
-
-for line in content:
-    number = int(line.strip())
-    factor1, factor2 = find_two_factors(number)
-    print(f"{number} = {factor1} * {factor2}")
+if __name__ == "__main__":
+    if len(argv) == 2:
+        factorize_rsa(argv[1])
--- factors.c
+++ factors.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success, -1 on failure
+ */
+int main(void)
 {
     long long int target_number = 239809320265259;
-    long int divisor = 2;
-    long int quotient;
+    long int smaller_factor = 2;
+    long int larger_factor;
 
-    while (target_number % divisor)
+    while (target_number % smaller_factor)
     {
-        if (divisor <= target_number)
+        if (smaller_factor <= target_number)
         {
-            divisor++;
+            smaller_factor++;
         }
-        else {
+        else
+        {
             return (-1);
         }
     }
 
-    quotient = target_number / divisor;
-    printf("%lld = %ld * %ld\n", target_number, quotient, divisor);
+    larger_factor = target_number / smaller_factor;
+    printf("%lld = %ld * %ld\n", target_number, larger_factor, smaller_factor);
     return (0);
 }
